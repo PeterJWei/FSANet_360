4318563ff6f890e1657e668108b905be35cdca5d
Chenye Yang <cy2540@columbia.edu>
2021-02-06 18:52:05 -0500

    add pre-trained models

--- training_and_testing/run_fsanet_train.sh
+++ training_and_testing/run_fsanet_train.sh
@@ -1,24 +1,24 @@
-# Train on protocal 1 Hospital
-# SSRNET_MT
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name 'Hospital'
+# # Train on protocal 1 Hospital
+# # SSRNET_MT
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name 'Hospital'
  
-# FSANET_Capsule
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name 'Hospital'
+# # FSANET_Capsule
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name 'Hospital'
 
 
-# FSANET_Netvlad
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name 'Hospital'
+# # FSANET_Netvlad
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name 'Hospital'
 
 
-# FSANET_Metric
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name 'Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name 'Hospital'
+# # FSANET_Metric
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name 'Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name 'Hospital'
 
 
 
@@ -46,74 +46,74 @@ KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --
 
 
 
-# Train on protocal 3 300WLP_Hospital
-# SSRNET_MT
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name '300WLP_Hospital'
- 
-# FSANET_Capsule
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name '300WLP_Hospital'
-
-
-# FSANET_Netvlad
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name '300WLP_Hospital'
-
-
-# FSANET_Metric
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name '300WLP_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name '300WLP_Hospital'
-
-
-# Train on protocal 4 ExtraPosePlus_Hospital
-# SSRNET_MT
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name 'ExtraPosePlus_Hospital'
- 
-# FSANET_Capsule
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name 'ExtraPosePlus_Hospital'
-
-
-# FSANET_Netvlad
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name 'ExtraPosePlus_Hospital'
-
-
-# FSANET_Metric
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name 'ExtraPosePlus_Hospital'
-KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name 'ExtraPosePlus_Hospital'
-
-
-
-# # Train on protocal 5 300WLP_ExtraPosePlus_Hospital
+# # Train on protocal 3 300WLP_Hospital
 # # SSRNET_MT
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name '300WLP_ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name '300WLP_Hospital'
  
 # # FSANET_Capsule
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name '300WLP_ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name '300WLP_Hospital'
 
 
 # # FSANET_Netvlad
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name '300WLP_ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name '300WLP_Hospital'
 
 
 # # FSANET_Metric
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name '300WLP_ExtraPosePlus_Hospital'
-# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name '300WLP_ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name '300WLP_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name '300WLP_Hospital'
+
+
+# # Train on protocal 4 ExtraPosePlus_Hospital
+# # SSRNET_MT
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name 'ExtraPosePlus_Hospital'
+ 
+# # FSANET_Capsule
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name 'ExtraPosePlus_Hospital'
+
+
+# # FSANET_Netvlad
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name 'ExtraPosePlus_Hospital'
+
+
+# # FSANET_Metric
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name 'ExtraPosePlus_Hospital'
+# KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name 'ExtraPosePlus_Hospital'
+
+
+
+# Train on protocal 5 300WLP_ExtraPosePlus_Hospital
+# SSRNET_MT
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 0 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 8 --nb_epochs 5 --model_type 1 --db_name '300WLP_ExtraPosePlus_Hospital'
+ 
+# FSANET_Capsule
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 2 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 3 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 4 --db_name '300WLP_ExtraPosePlus_Hospital'
+
+
+# FSANET_Netvlad
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 5 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 6 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 7 --db_name '300WLP_ExtraPosePlus_Hospital'
+
+
+# FSANET_Metric
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 8 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 9 --db_name '300WLP_ExtraPosePlus_Hospital'
+KERAS_BACKEND=tensorflow python FSANET_train.py --batch_size 16 --nb_epochs 5 --model_type 10 --db_name '300WLP_ExtraPosePlus_Hospital'
 
 
 
